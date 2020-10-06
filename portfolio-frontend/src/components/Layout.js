9db3db3b191867c6c6140b8353a17624a8a0cf80
SweDark <jesper-jeppe@hotmail.com>
2020-10-06 12:47:01 +0200

    update nav, footer, layout, sidebar + 404

--- portfolio-frontend/src/components/Layout.js
+++ portfolio-frontend/src/components/Layout.js
@@ -1,17 +1,19 @@
 import React from "react"
-// import "../css/main.css"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 const Layout = ({ children }) => {
-  return (
-    <>
-    <Navbar />
-      <h2>layout component</h2>
-      {children}
-      <Footer/>
-    </>
-  )
+const [isOpen, setIsOpen] = React.useState(false)
+const toggleSidebar = () => {
+setIsOpen(!isOpen)
 }
-
+return (
+<>
+<Navbar toggleSidebar={toggleSidebar} />
+<Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
+{children}
+<Footer />
+</>
+)
+} 
 export default Layout
